bc9de85eb6f46ba0ed42f9a8901b13ef23b5bca9
Rabinath Jha <rabinath.jha@datagrokr.com>
2019-08-30 16:55:42 +0530

    #dict: working front end

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,73 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import axios from 'axios';
+import ReactJson from 'react-json-view'
+class IndexPage extends React.Component {
 
-import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+  state = {
+    selectedFile: null,
+    uploadStatus: null,
+    searchKey: null,
+    searchResult: null,
+  };
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+  onChangeUploadHandler=event=>{
+    this.setState({
+      selectedFile: event.target.files[0],
+      loaded: 0,
+    })
+  }
+
+  onChangeSearchHandler = event => {
+    this.setState({
+      searchKey: event.target.value,
+      loaded: 0,
+    })
+  }
+
+  onClickUploadHandler = () => {
+    const data = new FormData() 
+    data.append('dictionaryTxt', this.state.selectedFile)
+
+    axios.post("http://localhost:5014/dictionary/upload", data)
+    .then(() => {
+      this.setState({uploadStatus:'success'})
+    })
+    .catch((err) => this.setState({uploadStatus:'faliure'}))
+  }
+
+  onClickSearchHandler = () => {
+    const { searchKey } = this.state;
+    axios.get(`http://localhost:5014/dictionary/search/${searchKey}`)
+    .then(data => {
+      this.setState({searchResult: data})
+    })
+    .catch((err) => {
+      this.setState({searchResult: err})
+    })
+  }
+
+  render(){
+
+    const {uploadStatus, searchResult} = this.state;
+
+    return(
+      <div>
+        <div>
+            <h2 style={{color:'purple'}}>Upload dictionary:</h2>
+            <input type="file" name="fileToUpload" id="fileToUpload" onChange={this.onChangeUploadHandler} />
+            <button type="button" name="upload button" onClick={this.onClickUploadHandler}>Upload</button>
+            {uploadStatus === 'success' && <h2 style={{color: 'green'}}>Successfuly uploaded</h2>}
+            {uploadStatus === 'faliure' && <h2 style={{color: 'red'}}>Upload unsuccessful</h2>}
+        </div>
+        <div>
+          <h2 style={{color:'blue'}}>Search words</h2>
+          <input type="text" name="fname" onChange={this.onChangeSearchHandler} />
+          <button type="button" name="search button" onClick={this.onClickSearchHandler}>Search</button>
+          {searchResult && <ReactJson style={{marginTop:'20px'}} src={searchResult.data} />}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default IndexPage
